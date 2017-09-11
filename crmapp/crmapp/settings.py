b5973d1742e298f0f248faef38979ff69b4d051c
Ankita Gupta <ankitagupta@Ankitas-MacBook-Pro.local>
2017-09-11 17:07:37 +0530

    created the home page view

--- crmapp/crmapp/settings.py
+++ crmapp/crmapp/settings.py
@@ -10,17 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from django.core.exceptions import ImproperlyConfigured
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-def get_env_variable(var_name):
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "Set the %s environment variable" % var_name
-        raise ImproperlyConfigured(error_msg)
 
-ENV_ROLE = get_env_variable('ENV_ROLE')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -28,12 +20,10 @@ ENV_ROLE = get_env_variable('ENV_ROLE')
 SECRET_KEY = 'al$2c%wlb&z(la@tbfjl7t&9dn#xg2q^z#!o+9u14=e!gdzg!='
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-if ENV_ROLE == 'development':
-    DEBUG = True
-    TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DEBUG = True
+
 ALLOWED_HOSTS = []
 
 
@@ -46,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crmapp.marketing',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +92,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
