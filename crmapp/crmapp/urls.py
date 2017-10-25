908c9299cebf0fe869f2b611595975604a89666f
Ankita Gupta <ankitagupta@Ankitas-MacBook-Pro.local>
2017-10-25 10:40:08 +0530

    created account list view

--- crmapp/crmapp/urls.py
+++ crmapp/crmapp/urls.py
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
     (r'^logout/$',
         'django.contrib.auth.views.logout', {'next_page': '/login/'}
     ),
-    (r'^account/list/$', AccountList.as_view(),name="account_list"),
+    url(r'^account/list/$', AccountList.as_view(),name="account_list"),
 )
